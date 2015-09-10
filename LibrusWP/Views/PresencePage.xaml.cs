7edcd6f1d3ca3121732f0084a17517db35c88403
damsaneta <damsaneta@gmail.com>
2015-09-10 02:45:21 +0200

    działająca app

--- LibrusWP/Views/PresencePage.xaml.cs
+++ LibrusWP/Views/PresencePage.xaml.cs
@@ -34,7 +34,32 @@ namespace LibrusWP.Views
                 string[] tab = value.Split(new char[] { '/' });
                 this.DataContext = new PresencePageViewModel(LibrusFactory.CreateLibrusManager(), tab[0], tab[1], tab[2]);
             }
+            if (this.State.ContainsKey("presences"))
+            {
+                string[] tab = ((string)this.State["presences"]).Split(new char[] { '/' });
+                int i =0;
+                foreach(var p in this.ViewModel.Presences)
+                {
+                    p.Present =  Convert.ToBoolean(tab[i]);
+                    i++;
+                }
+              //  this.ViewModel.Classes.Where(x => x.Id == (string)this.State["selectedClass"]).SingleOrDefault().IsSelected = true;
+            }
 
+        }
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            if (e.NavigationMode != System.Windows.Navigation.NavigationMode.Back
+            && e.NavigationMode != System.Windows.Navigation.NavigationMode.Forward)
+            {
+                string presences= string.Empty;
+                foreach(var p in this.ViewModel.Presences)
+                {
+                    presences = presences + p.Present.ToString() +"/";
+                }
+                this.State["presences"] = presences;
+            }
         }
 
         private void ZapiszButtonClick(object sender, RoutedEventArgs e)
